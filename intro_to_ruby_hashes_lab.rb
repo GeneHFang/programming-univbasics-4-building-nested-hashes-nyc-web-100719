66e93438dd888aee2624bbd08db632e9193bc663
Gene H Li <ghl234@nyu.edu>
2019-09-18 21:09:03 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -39,7 +39,7 @@ def monopoly_with_third_tier
   hash[:railroads][:names] = {
     reading_railroad: "",
     pennsylvania_railroad: "",
-    name3: "",
+    b_and_o_railroad: "",
     name4: ""
   }
   
