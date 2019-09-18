25f07a4ae0aba4b67646046fb238b6fd0016b8f6
Gene H Li <ghl234@nyu.edu>
2019-09-18 21:00:07 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -10,7 +10,9 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   
-  base_hash[:railroads].store(:pieces, {})
+  hash = base_hash[:railroads]
+  
+  hash.store(:pieces, Hash.new )
   
   
   puts base_hash
